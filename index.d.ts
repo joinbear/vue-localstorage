e9c514402e1d84e0a801a0283e274fc4070a73d4
qiaoni <123456789>
2020-06-17 17:06:00 +0800

    新增index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+import Vue, { PluginFunction } from 'vue';
+
+interface VueLocalStorage extends PluginFunction<any> {
+  install(Vue: Vue): void
+  setSession(name: string, value: any): void
+  getSession(name: string): any
+  setStorage(name: string, value: any, expire: number): void
+  getStorage(name: string): any
+  removeStorage(name: string): void
+  keyStorage(index: number): any
+  clearAll(): void
+  setCookie(name: string, value: string, expire?: number, domain?: string, path?: string): any
+  getCookie(name: string): string
+  clearCookie(name: string): void
+}
+
+
+declare const localStroage: VueLocalStorage;
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $localStorage: VueLocalStorage;
+  }
+}
+export default localStroage;
+export as namespace localStroage;
